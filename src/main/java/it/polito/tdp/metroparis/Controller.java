618111bfeaa5de828e7953e851df4d5053152c7c
steph <steph@LAPTOP-P3GM883F.home-life.hub>
2023-05-25 15:27:03 +0200

    ,

--- src/main/java/it/polito/tdp/metroparis/Controller.java
+++ src/main/java/it/polito/tdp/metroparis/Controller.java
@@ -1,13 +1,19 @@
 package it.polito.tdp.metroparis;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.metroparis.model.Fermata;
+import it.polito.tdp.metroparis.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 
 public class Controller {
+	
+	private Model model;
 
     @FXML
     private ResourceBundle resources;
@@ -16,21 +22,39 @@ public class Controller {
     private URL location;
 
     @FXML
-    private ComboBox<?> boxArrivo;
+    private ComboBox<Fermata> boxArrivo;
 
     @FXML
-    private ComboBox<?> boxPartenza;
+    private ComboBox<Fermata> boxPartenza;
 
     @FXML
     private TextArea txtResult;
 
     @FXML
     void handleCalcola(ActionEvent event) {
+    	
+    	Fermata partenza = boxPartenza.getValue();
+    	Fermata arrivo = boxArrivo.getValue();
+    	
+    	if (partenza != null && arrivo != null && !partenza.equals(arrivo) ) {
+    		List<Fermata> percorso = model.percorso(partenza,  arrivo);
+    		txtResult.setText("Percorso tra " + partenza.getNome()+ " e " + arrivo.getNome()+"\n" );
+    		
+    		for (Fermata f : percorso) {
+    			txtResult.appendText(f.getNome()+"\n");
+    		}
+    	}else {
+    		txtResult.setText("Devi selezionare due stazioni diverse tra loro \n");
+    	}
 
     }
 
     @FXML
     void handleCrea(ActionEvent event) {
+    	this.model.creaGrafo();
+    	if (this.model.isGrafoLoaded()) {
+    		txtResult.setText("Grafo correttamente importato");
+    	}
 
     }
 
@@ -41,5 +65,14 @@ public class Controller {
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Metro.fxml'.";
 
     }
+
+	public void setModel(Model m) {
+		// TODO Auto-generated method stub
+		this.model = m;
+		List<Fermata> fermate = this.model.getAllFermate();
+		boxPartenza.getItems().setAll(fermate);
+		boxArrivo.getItems().setAll(fermate);
+		
+	}
 
 }
--- src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
+++ src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
@@ -6,10 +6,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.javadocmd.simplelatlng.LatLng;
 
 import it.polito.tdp.metroparis.model.Connessione;
+import it.polito.tdp.metroparis.model.CoppieF;
 import it.polito.tdp.metroparis.model.Fermata;
 import it.polito.tdp.metroparis.model.Linea;
 
@@ -67,6 +69,162 @@ public class MetroDAO {
 		}
 
 		return linee;
+	}
+	
+	//METODO che permette di capire se due vertici sono connessi 
+	//--> se il valore restituito dalla quesry e' uguale a zero allora il metodo restituisce false = non ci sono connessione altrimenti vero
+	public boolean isConnesse(Fermata partenza, Fermata arrivo) {
+		// TODO Auto-generated method stub
+		
+		String sql = "SELECT COUNT(*) AS C "
+				+ "FROM connessione "
+				+ "WHERE id_stazP = ? AND id_stazA = ?";
+		
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			st.setInt(2, arrivo.getIdFermata());
+			
+			ResultSet rs = st.executeQuery();
+			
+			//rs ha un solo elemento quindi niente while
+			rs.first();
+			
+			int c = rs.getInt("c");
+			
+			conn.close();
+			
+			return c!=0;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
+	
+	}
+
+	/*public List<Fermata> trovaCollegate(Fermata partenza) {
+		// TODO Auto-generated method stub
+		
+		//creare una query piu' efficiente --> data una stazione di partenza, ottenere tutte le stazioni di arrivo
+		//IN QUESTO MODO IL LAVORO VIENE FATTO TUTTO DAL DB
+		
+		
+		//!!! ATTENZIONE : la query RESTITUISCE ID  e non l'oggetto fermata 
+		//--> necessario una "CONVERSIONE"
+		
+		//2 MODI : 
+		         //1)nel DB : fare un join tra tabella connessioni e fermata
+		
+		String sql = "SELECT * "
+				+ "FROM fermata "
+				+ "WHERE id_fermata IN ( "
+				+ "SELECT id_stanzA "
+				+ "FROM connessione "
+				+ "WHERE id_stazP = ? "
+				+ "GROUP BY id_stazA ) "
+				+ "ORDER BY nome ASC"; 
+		List<Fermata> fermate = new ArrayList<>();
+		try  {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+		
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				Fermata f = new Fermata(res.getInt("id_fermata"), res.getString("nome"), new LatLng(res.getDouble("coordX"), res.getDouble("coordY")));
+				fermate.add(f);
+			}
+			res.close();
+			st.close();
+			conn.close();
+			
+			return fermate;
+			
+		}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+	
+		
+		
+		
+		         
+
+	}	*/
+	
+//2)in JAVA : trucco de IDENTITY MAP
+	public List<Fermata> trovaIdCollegate(Fermata partenza, Map<Integer, Fermata> fermateIdMap) {
+		
+		String sql = "SELECT id_stazA "
+				+ "FROM connessione "
+				+ "WHERE id_stazP = ? "
+				+ "GROUP BY id_stazA "; 
+		
+		List<Fermata> fermate = new ArrayList<>();
+		try  {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+		
+			ResultSet res = st.executeQuery();
+			
+			while (res.next()) {
+				Integer idFermata = res.getInt("id_stazA");
+				fermate.add(fermateIdMap.get(idFermata));
+			}
+			res.close();
+			st.close();
+			conn.close();
+			
+			return fermate;
+			
+		}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+	
+	}
+	
+	public List<CoppieF> getAllCoppie(Map<Integer, Fermata> fermatIdMap) {
+		
+		String sql = "SELECT distinct id_stazP, id_stazA "
+				+ "FROM connessione ";
+		
+		List<CoppieF> allCouple = new ArrayList<>();
+		
+		try {
+			
+			Connection conn = DBConnect.getConnection();
+			
+			PreparedStatement st = conn.prepareStatement(sql);
+			
+
+			ResultSet res = st.executeQuery();
+			
+			while (res.next()) {
+				
+				CoppieF coppia = new CoppieF(fermatIdMap.get(res.getInt("id_stazP")), fermatIdMap.get(res.getInt("id_stazA"))) ; 
+				allCouple.add(coppia);
+			}
+			
+			st.close();
+			conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		return allCouple;
+		
 	}
 
 	
